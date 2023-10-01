6db283ac34882959aef23dd00b3e27377dfb06f9
nirual81 <116461643+nirual81@users.noreply.github.com>
2023-10-01 20:28:52 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+#
+#    Gnome Shell Settings
+#
+
+run_gnome_extension_config() {
+    name="$1"
+    folder="$2"
+    xml="$3"
+
+}
+
+read -p "Sollen gnome-tweaks Konfiguriert werden? (J/n): " choice
+choice=${choice:-'J'}
+if [ "$choice" = "J" ] || [ "$choice" = "j" ]; then
+    ext="~/.local/share/gnome-shell/extensions/"
+    
+    read -p "Soll Dash-To-Panel konfiguriert werden? (J/n): " choice
+    choice=${choice:-'J'}
+    if [ "$choice" = "J" ] || [ "$choice" = "j" ]; then
+        echo "Nvidia-Treiber werden installiert"
+        sudo ubuntu-drivers autoinstall
+    else
+        read -p "Möchten Sie den Nvidia-Treiber über apt installieren? (J/n: " choice
+        choice=${choice:-'J'}
+        if [ "$choice" = "J" ] || [ "$choice" = "j" ]; then
+            echo "Nvidia-Treiber werden über apt installiert"
+            sudo apt install nvidia-driver-535 nvidia-dkms-535
+        else
+            echo "Nvidia-Treiber wird nicht installiert."
+        fi
+    fi
+
+    
+else
+    echo "Running Setup"
+fi
+
+
+#
+#    Setup Skripts
+#
+
 # Funktion zur Installation des Nvidia-Treibers
 install_nvidia_driver() {
     read -p "Möchten Sie den Nvidia-Treiber über den Ubuntu-Autoinstaller installieren? (J/n): " choice
